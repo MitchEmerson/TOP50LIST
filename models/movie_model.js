c7d65386f16d74ccdac4e714e99d646899778a3a
Mitch Emerson <mitchemerson1984@hotmail.com>
2021-06-03 12:26:36 -0700

    Added a generic create route and middleware, fleshing out an Express app, structure is coming together

---
+++ models/movie_model.js
@@ -0,0 +1,27 @@
+//FAKE-A-BASE
+
+const movies = [
+    {
+        name:'Star Wars',
+        year: 1977,
+        goodMovie: true
+    },
+    {
+        name:'Back To The Future',
+        year: 1985,
+        goodMovie: true
+    },
+    {
+        name:'Fargo',
+        year: 1995,
+        goodMovie: true
+    }
+];
+
+
+
+
+
+
+
+module.exports = movies;
--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+const movies = require('./models/movie_model.js');
+const PORT = 3000;
 
-app.get('/somedata', (req, res) => {
-    res.send('here is your information');
+app.get('/movies/:movieIndex', function(req, res){
+    res.render('show.ejs');
+});
+
+// routes/controllers
+// show route
+// this route will catch GET requests to /movies/anyValue
+// and respond with a single movie
+app.get('/movies/:movieIndex', (req, res) => {
+    res.send(movies[req.params.movieIndex]);
+});
+// index route
+// this route will catch GET requests to /fruits
+// and respond with all the fruits
+app.get('/movies/', (req, res) => {
+    res.send(movies);
+});
+
+// create route
+// this route will catch POST requests to /fruits
+// and, after creating new data, respond by redirecting
+// the user to the index route
+app.post('/movies/', (req, res) => {
+    if(req.body.goodMovie === 'on'){
+        req.body.goodMovie = true;
+    } else {
+        req.body.goodMovie = false;
+    }
+    movies.push(req.body);
+    console.log(movies);
+    res.send('data received');
 });
 
 
 
-app.listen(3000, ()=>{ 
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => { 
     console.log('do this thing')
 });
