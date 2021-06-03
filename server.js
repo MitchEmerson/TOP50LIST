b9e812c66dabf71ee5e0be9076894c952fe11421
Mitch Emerson <mitchemerson1984@hotmail.com>
2021-06-03 10:37:10 -0700

    Setting up some basic file structure, created my .gitignore, server.js, and created an instance of express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/somedata', (req, res) => {
+    res.send('here is your information');
+});
+
+
+
+app.listen(3000, ()=>{ 
+    console.log('do this thing')
+});
