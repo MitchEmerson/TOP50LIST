315422e97f75c0633cba4d28faabf437aa2b6bff
Mitch Emerson <mitchemerson1984@hotmail.com>
2021-06-07 12:16:47 -0700

    added some routes, trouble with edit route

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -6,12 +6,18 @@ const router = express.Router();
 const comment = require("../models/Comment.js");
 
 
-
+router.get('/showComment', (req,res) => {
+    res.render('commentViews/showComment.ejs')
+});
 
 
 router.get('/newComment', (req, res) => {
     res.render('commentViews/newComment.ejs'
 )});
+
+router.get('/edit', (req,res) => {
+    res.render('commentViews/edit.ejs')
+});
 
 
 module.exports = router;
