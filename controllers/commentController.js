c399f8a0faab11e3bc595ee11daa8258fe86bc9b
Mitch Emerson <mitchemerson1984@hotmail.com>
2021-06-09 22:31:06 -0700

    Finished edit and delete, I think I hit MVPgit add -A

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -16,8 +16,33 @@ router.get('/newComment', (req, res) => {
     res.render('commentViews/newComment.ejs'
 )});
 
-router.get('/edit', (req,res) => {
-    res.render('commentViews/edit.ejs')
+router.get('/:id', (req,res) => {
+    db.Comment.findById(req.params.id, function(err,foundComment) {
+        if(err) res.send(err);
+     
+        const context = {comment: foundComment}
+        res.render('commentViews/edit.ejs', context)
+
+    })
+    
+});    
+
+router.put('/:id', (req,res) =>  {
+    console.log(req.body.comment)
+    db.Comment.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set: {
+        comment: req.body.comment
+            },
+        },
+        { new: true },
+        function (err, updatedComment) {
+            if(err) return res.send(err); 
+            console.log(err)
+            return res.redirect('/');
+        }
+    );
 });
 
 router.post('/new', async (req,res) => {
@@ -29,6 +54,14 @@ router.post('/new', async (req,res) => {
     console.log("new comment:", result)
     res.redirect(`/movie/${newComment.movieIndex}`)
 });
+
+router.delete('/:id', (req,res) => {
+    db.Comment.findByIdAndDelete(req.params.id, function(err, deletedComment) {
+        if(err) return res.send(err)
+       return res.redirect("/")
+    })
+
+})
 
 
 module.exports = router;
